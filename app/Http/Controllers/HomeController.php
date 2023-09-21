ed32a5b8c794deac6c02a2f70d601bf19744922e
ayoub-atif <ayoubatif449@gmail.com>
2023-09-21 12:32:31 +0100

    finish auth

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view("home");
+    }
+    public function cart(){
+        return view("frontend.cart");
+    }
+    public function contact(){
+        return view("frontend.contact");
+    }
+    public function login(){
+        return view("frontend.login");
+    }
+    public function category(){
+        return view("frontend.category");
+    }
+   
+    
+    
+    
+}
